9581e6b7fecd63aaf69a46361b0ce51d47c824b4
lasha tatulashvili <lashatatu@gmail.com>
2022-04-03 17:25:04 +0400

    tests jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe("Test GET /launches", () => {
+	test("it should respond with 200 success", () => {
+		const response = 200;
+		expect(response).toBe(200)
+	});
+});
+
+describe("Test POST /launch",()=>{
+	test("it should respond with 200 success",()=>{
+
+	})
+	test('It should catch missing required properties',()=>{})
+	test('It should catch invalid dates',()=>{})
+})
