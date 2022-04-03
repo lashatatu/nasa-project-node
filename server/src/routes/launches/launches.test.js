3c005f3b69a88f337bc0dac9542fadbd09114067
lasha tatulashvili <lashatatu@gmail.com>
2022-04-03 18:16:06 +0400

    supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,11 +11,35 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launch", () => {
-	test("it should respond with 200 success", () => {
+	const completeLaunchData = {
+		mission: "uss enterprise",
+		rocket: "NCC 1701-D",
+		target: "kepler-186 f",
+		launchDate: "January 4, 2027",
+	};
+	const completeDataWithoutDate = {
+		mission: "uss enterprise",
+		rocket: "NCC 1701-D",
+		target: "kepler-186 f",
+	};
 
+	test("it should respond with 201 created", async () => {
+		const response = await request(app)
+		  .post("/launches")
+		  .send(completeLaunchData)
+		  .expect("Content-Type", /json/)
+		  .expect(201);
+
+		const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+		const responseDate = new Date(response.body.launchDate).valueOf();
+		expect(responseDate).toBe(requestDate);
+
+		expect(response.body).toMatchObject(completeDataWithoutDate);
 	});
+
 	test("It should catch missing required properties", () => {
 	});
+
 	test("It should catch invalid dates", () => {
 	});
 });
