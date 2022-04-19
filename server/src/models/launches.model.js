0e616f05faa47158a904406a8a1f473bd85b4c6b
lasha tatulashvili <lashatatu@gmail.com>
2022-04-19 10:52:23 +0400

    auto increment

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -33,6 +34,14 @@ async function getAllLaunches () {
 }
 
 async function saveLaunch (launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if(!planet){
+    throw new Error('Planet not found');
+  }
+
   await launchesDatabase.updateOne({
     flightNumber: launch.flightNumber,
   }, launch, {
