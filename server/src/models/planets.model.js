0f1ee8f6b7ae43227008b4d8d6ea804d52cb8153
lasha tatulashvili <lashatatu@gmail.com>
2022-04-19 09:58:51 +0400

    finding documents

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const {parse} = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const habitablePlanets = [];
 
 function isHabitablePlanet (planet) {
@@ -20,11 +22,14 @@ function loadPlanetsData () {
     fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')).pipe(parse({
       comment: '#',
       columns: true,
-    })).on('data', (data) => {
-      if ( isHabitablePlanet(data) ) {
-        habitablePlanets.push(data);
-      }
-    }).on('error', (err) => {
+    }))
+      .on('data',async (data) => {
+        if ( isHabitablePlanet(data) ) {
+          // await planets.create({
+          //   keplerName: data.kepler_name,
+          // } );
+        }
+      }).on('error', (err) => {
       console.log(err);
       reject(err);
     }).on('end', () => {
